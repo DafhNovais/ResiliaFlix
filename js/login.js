28c069144b3a71a53cc6181f43142724ddac521c
Mariana Santana <s.santana.mariana@gmail.com>
2021-11-05 17:33:40 -0300

    Revert "finalização do login"
    This reverts commit eec1e6647506bfe29fbd9affc0725ff0d54d8afd.

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
-
-function validarEmail(email) {
-    usuario = email.value.substring(0, email.value.indexOf("@"));
-    dominio = email.value.substring(email.value.indexOf("@")+ 1, email.value.length);
+function validacaoEmail(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    console.log(usuario);
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+    console.log(dominio);
     if ((usuario.length >=1) &&
         (dominio.length >=3) &&
         (usuario.search("@")==-1) &&
@@ -11,24 +12,8 @@ function validarEmail(email) {
         (dominio.search(".")!=-1) &&
         (dominio.indexOf(".") >=1)&&
         (dominio.lastIndexOf(".") < dominio.length - 2)) {
-            document.getElementById("msgemail").innerHTML="<p style='color:#1eff00; font-weight:bold' >Email válido </p>";
-            return;    
+    document.getElementById("msgemail").innerHTML="E-mail válido";
+    } else{
+    document.getElementById("msgemail").innerHTML="<font color='red'>Email inválido </font>";
     }
-    else{
-        document.getElementById("msgemail").innerHTML="<p style='color:#ff0000; font-weight:bold' >Email inválido </p>";
-
-    } 
-}   
-function validarSenha(senha){
-
-    var senha = formuser.senha.value;
-
-    if(senha != " " && senha.length >= 6 && senha.length <= 8){
-        document.getElementById("msgsenha").innerHTML="<p style='color:#1eff00; font-weight:bold; margin-botton:0' >Senha válida </p>"
-        return;       
-    }
-    else{
-        document.getElementById("msgsenha").innerHTML="<p style='color:#ff0000; font-weight:bold' >Senha inválida</p>"; 
-        alert('Senha inválida, necessário conter entre 6 a 8 caracteres')  
-    }   
 }
